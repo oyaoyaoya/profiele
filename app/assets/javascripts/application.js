1276b37a4b20a1ff0f6331065fd1a5c702bcc97e
Oyana <oyana@oya.local>
2016-03-24 22:05:52 +0900

    js追加しました

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,39 @@
 $(function(){
-   // #で始まるアンカーをクリックした場合に処理
-   $('a[href^=#]').click(function() {
-      // スクロールの速度
-      var speed = 400; // ミリ秒
-      // アンカーの値取得
-      var href= $(this).attr("href");
-      // 移動先を取得
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      // 移動先を数値で取得
-      var position = target.offset().top;
-      // スムーススクロール
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-   });
+
+
+  ///初期の設定
+  var ore = $('#ore');
+  var ure = $('#ure');
+  $(window).on('scroll',function(){
+  	// スクロール量を取得
+  	var y = $(window).scrollTop();// Y方向 (縦)
+    console.log(y);	//所得済み
+
+    var move = function(){
+      ore.animate({'left':'2px'},2000);
+      ure.animate({'left':'2px'},2000);
+    }
+
+    if(y >= 200){
+      move();
+    }
+
+  });
+
+
+//クリック時に暗くなる
+var mlink = $("#mlink");
+var blink =$("#blink");
+
+mlink.on('click',function(){
+  alert(mlink);
+  $(this).css('-webkit-filter','grayscale(70%)');
+  blink.css('-webkit-filter','');
+});
+
+blink.on('click',function(){
+   $(this).css('-webkit-filter','grayscale(70%)');
+   mlink.css('-webkit-filter','');
+});
+
 });
